5324095b1fe1705083cbf29ea22cedc7275d0b48
heartandhandstraining <heartandhandstraining@gmail.com>
2019-04-24 17:12:20 +0000

    added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'vikingsheepart@yahoo.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Devmatch Contact Form Message')
+  end
+end
