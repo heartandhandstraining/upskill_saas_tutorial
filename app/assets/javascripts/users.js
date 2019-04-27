d441aec229902e09877c03370cf7db01b72d1d54
heartandhandstraining <heartandhandstraining@gmail.com>
2019-04-27 22:27:42 +0000

    added Stripe response handler and error handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,8 +8,9 @@ $(document).on('turbolinks:load', function(){
   
   // When user clicks form submit btn
   submitBtn.click(function(event){
-    // prevent default submission behavior.
+    // Javascript prevent default submission to us behavior.
     event.preventDefault()
+    submitBtn.val("Processing").prop('disabled', true);
     
     // Collect credit card fields.
     var ccNum = $('#card_number').val(),
@@ -17,21 +18,55 @@ $(document).on('turbolinks:load', function(){
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
         
-    // Send that info to Stripe and make function to send listed info & define what we will do with
-    // card token Stripe will return
-  Stripe.createToken({
-    number: ccNum, cvc: cvcNum,
-    exp_month: expMonth,
-    exp_year: expYear
+    // Use Stripe JS library to check for card errors. 
+    var error = false;
     
-  },stripeResponseHandler);
+    // Validate card number.
+    if(!Stripe.card.validateCardNumber(ccNum)){
+      error = true;
+      alert('The credit card appears to be invalid.')
+      
+    }
+    
+    // Validate cvc number.
+    if(!Stripe.card.validateCVC(cvcNum)){
+      error = true;
+      alert('The credit card appears to be invalid.')
+      
+    }
+    
+    // Validate exp date.
+    
+    if(!Stripe.card.validateExpiry(expMonth, expYear){
+      error = true;
+      alert('The expiration date appears to be invalid.')
+      
+    }
+      
+      if (error) {
+      // don't send to Stripe, so customer can fix
+      submitBtn.prop('disabled', false).val("Sign Up")
+    } else {  
+      // Send that info to Stripe and make function to send listed info, so cust can fix 
+      Stripe.createToken({
+        number: ccNum, cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+        // & define what we will do with card token Stripe will return, defined below
+      }, stripeResponseHandler);
+    }
+    
+      return false;
+    });
   
-  });
-
-  
-  // Collect credit card fields.
-  // Send that info to stripe.
-  // Stripe will return a card token 
-  // that we need to inject as hidden field to form
-  // Submit form to Rails app.
+  //Stripe will retur a card token
+  function stripeResponseHandler(status, response) {
+    //what Strip sends back will be converted to this token/id
+    var token = response.id;
+    // inject it as hidden field to form
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    
+    // Submit form to Rails app.
+    theForm.get(0).submit();
+    }
 });
