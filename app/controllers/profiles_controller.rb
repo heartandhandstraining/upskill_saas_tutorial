1f4e90822b7ca564de7cd9cec79ff564de59151d
heartandhandstraining <heartandhandstraining@gmail.com>
2019-05-03 03:36:29 +0000

    added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
   # User makes GET to /users/:user_id/profile/new
   def new
     #Render blank profile details form
+    @profile = Profile.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190501011625) do
+ActiveRecord::Schema.define(version: 20190503025436) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -28,16 +28,15 @@ ActiveRecord::Schema.define(version: 20190501011625) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.integer "user_id"
-    t.string  "first_name"
-    t.string  "last_name"
-    t.string  "job_title"
-    t.string  "phone_number"
-    # t.string  "phone_number"
-    # t.string  "contact_email"
-    # t.text    "description"
-    # t.datetime "created_at",     null: false
-    # t.datetime "updated_at",     null: false
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,3 +59,6 @@ ActiveRecord::Schema.define(version: 20190501011625) do
   end
 
 end
+
+# If need to start over on a table, can delete table and re-create and migrate
+# https://stackoverflow.com/questions/38711156/deleting-table-from-schema-rails
